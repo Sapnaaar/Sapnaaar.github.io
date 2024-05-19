097aaca5c02f9e4571a295dcce11ef5ce4f152f3
Sapnaaar <s4005596@student.rmit.edu.au>
2024-05-20 00:35:32 +1000

    assignment 2 update 3

--- assignment2/script.js
+++ assignment2/script.js
@@ -94,19 +94,67 @@ function toggleFullScreen() {
 // function toggleVolume(){
 
 // like button------------------------------------------------------------------------
-// const heartButton = document.querySelector("#heart-button");
-// console.log(heartButton);
+const heartButton = document.querySelector("#heart-button");
+console.log(heartButton);
 
-// heartButton.addEventListener("click", toggleHeart);
-// might have to change?
-function changeImage() {
-  if (
-    document.getElementById("heartImage").src ==
-    "https://img.icons8.com/ios/50/like--v1.png"
-  ) {
-    document.getElementById("heartImage").src = "heart-filled.png";
+heartButton.addEventListener("click", toggleHeart);
+
+const heartImg = document.querySelector("#heart-img");
+console.log(heartImg);
+
+function toggleHeart() {
+  if (heartImg.src.match("heart-outlined.png")) {
+    heartImg.src = "heart-filled.png";
+    showMessage("Video has been added to favourites!");
   } else {
-    document.getElementById("heartImage").src =
-      "https://img.icons8.com/ios/50/like--v1.png";
+    heartImg.src = "heart-outlined.png";
+    clearMessage();
   }
+}
+
+function showMessage(message) {
+  messageDiv.textContent = message;
+}
+
+function clearMessage() {
+  messageDiv.textContent = "";
+}
+
+// having text pop up when favouriting the video
+const messageDiv = document.querySelector("#message");
+console.log(message);
+
+heartButton.addEventListener("click", addToFavourites);
+
+function addToFavourites() {
+  messageDiv.textContent = "(video has been added to favourites!)";
+}
+
+// Originally I attempted to do it by using "document.getelementByiD" to link the images to javascript in order to to have an image switch upon a click - however I realised it didnt feel interactive since unlike a button the icon will not be able to indicate its clickable when the mouse hovers over it, and so as a result I decided to change the code and make it a button instead which has the function of changing icons when clicked by a UserActivation.
+
+// function changeImage() {
+//   if (
+//     document.getElementById("heartImage").src ==
+//     "https://img.icons8.com/ios/50/like--v1.png"
+//   ) {
+//     document.getElementById("heartImage").src = "heart-filled.png";
+//   } else {
+//     document.getElementById("heartImage").src =
+//       "https://img.icons8.com/ios/50/like--v1.png";
+//   }
+// }
+
+// rewind and fast forward button
+const rewindButton = document.querySelector("#rewind-button");
+const fastForwardButton = document.querySelector("#fast-forward-button");
+
+rewindButton.addEventListener("click", rewindVideo);
+fastForwardButton.addEventListener("click", fastForwardVideo);
+
+function rewindVideo() {
+  video.currentTime -= 15;
+}
+
+function fastForwardVideo() {
+  video.currentTime += 15;
 }
